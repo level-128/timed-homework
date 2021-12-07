77e3ad48ff15645a2f5bb5e160d11c674aba1250
level-128 <wang740305@outlook.com>
2021-12-07 16:35:39 +0800

    formatted code

--- FoodOrderMgntSystem.c
+++ FoodOrderMgntSystem.c
@@ -13,8 +13,9 @@
 #define $(list__, index__) list_get_node(list__, index__)->value
 
 
-static int DEFAULT_DICT_SIZE = 500;
-static double REHASH_LIMIT = 0.7;
+const static int DEFAULT_DICT_SIZE = 500;
+
+const static double REHASH_LIMIT = 0.7;
 
 
 void ERROR(char * message) {
@@ -39,26 +40,58 @@ print(next(x))
 
  */
 // if you want to run this code, it is strongly recommended to use pypy, instead of cpython, as Python interpreter.
-// this prime will be used in the hash function as the modular.
+// this prime will be used in the dict_hash function as the modular.
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// dictionary object
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * memory module of the dictionary:
+ *
+ * 0        1        2        3         4                                    8 bytes
+ * ---------------------------------------------------------------------------
+ * |             size                   |         content_count              | <- 0x00
+ * ---------------------------------------------------------------------------
+ * |                             rand_salt                                   | <- 0x08
+ * ---------------------------------------------------------------------------
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+
+
+
+
+// the individual trunk in the dictionary.
 typedef struct {
-	 bool is_used;
-	 bool is_at_default_location;
-	 uint32_t hash_value;
+	 bool is_used; // true if the chunk is empty, false otherwise.
+	 bool is_at_default_location; // true   if the chunk stores itself at the index which it should belong.
+	                              // false  if the index which it should belong has been occupied by another chunk.
+	 uint32_t hash_value; // the hash value of the key
 	 char * ptr_key;
 	 void * ptr_value;
 } dict_chunk;
 
+
 typedef struct {
-	 uint32_t size;
-	 uint32_t content_count;
-	 uint64_t rand_salt;
+	 uint32_t size; // the size of the dictionary
+	 uint32_t content_count; // how many chunk are there in the dictionary
+	 uint64_t rand_salt; // the salt of the hash function, generated per dict object.
 	 dict_chunk * dict_value;
 } dict;
 
+uint32_t dict_hash(const char * key, uint32_t seed);
+
+
 void dict_store(dict *, char *, void *);
 
-uint32_t hash(const char * key, uint32_t seed) {
+
+
+uint32_t dict_hash(const char * key, uint32_t seed) {
 	uint64_t result = 0;
 	for (uint32_t i = 0;; i++) {
 		if (key[i] == 0) {
@@ -105,7 +138,7 @@ void dict_store(dict * self, char * key, void * value) {
 	if (self->content_count > REHASH_LIMIT * self->size) {
 		dict_remap(self, self->size + self->size / 2);
 	}
-	uint32_t hash_value = hash(key, self->rand_salt);
+	uint32_t hash_value = dict_hash(key, self->rand_salt);
 	uint32_t location = hash_value % self->size;
 	dict_chunk * self_trunk = self->dict_value + location;
 	if (self_trunk->is_used && self_trunk->hash_value != hash_value) {
@@ -135,7 +168,7 @@ void dict_store(dict * self, char * key, void * value) {
 
 dict_chunk * p__dict_find_trunk(dict * self, char * key) {
 
-	uint32_t hash_value = hash(key, self->rand_salt);
+	uint32_t hash_value = dict_hash(key, self->rand_salt);
 	uint32_t location = hash_value % self->size;
 	dict_chunk * self_trunk = self->dict_value + location;
 	if (!self_trunk->is_used) {
@@ -155,7 +188,8 @@ dict_chunk * p__dict_find_trunk(dict * self, char * key) {
 			if (self_trunk->hash_value == hash_value) {
 				return self_trunk;
 			}
-		} else {
+		}
+		else {
 			ERROR("The requested trunk does not exist.");
 		}
 	}
@@ -165,8 +199,8 @@ void * dict_find(dict * self, char * key) {
 	return p__dict_find_trunk(self, key)->ptr_value;
 }
 
-// return the value and delete the key, return nu
 void dict_del(dict * self, char * key) {
+	// delete the record of the key-value pair in dictionary. Does not free the value.
 	if ((self->content_count < REHASH_LIMIT * self->size / 2) && self->size > DEFAULT_DICT_SIZE) {
 		dict_remap(self, (size_t) (self->size / 1.5));
 	}
@@ -177,6 +211,7 @@ void dict_del(dict * self, char * key) {
 }
 
 void dict_free(dict * self) {
+	// free the dictionary.
 	dict_chunk * self_trunk;
 	for (uint32_t location = 0; location < self->size; location++) {
 		self_trunk = self->dict_value + location;
@@ -187,20 +222,51 @@ void dict_free(dict * self) {
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// define a link list
+// link list
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// node in the list.
 typedef struct node {
-	 void * value;
+	 void * value; // stored value
 	 struct node * next_node;
 } node;
 
+// list
 typedef struct {
-	 size_t len;
-	 node * last_node;
-	 node * first_node;
-	 size_t last_visit_node_index;
-	 node * last_visit_node;
+	 size_t len; // the length of the linked list
+	 node * last_node; // the addr of the last node
+	 node * first_node; // the addr of the first node
+	 size_t last_visit_node_index; // record the last visited index from method 'list_get_node'
+	 node * last_visit_node; // record the last visited node from method 'list_get_node'
 } list;
+
+list * new_list(void);
+// create a new list
+
+list * new_list_init(uint32_t param_len, char * params[]);
+// create a new list with elements 'params[]' pre initialized in the list.
+
+node * list_get_node(list * self, size_t index);
+// return the node in the list at index 'index'.
+
+void list_append(list * self, void * newval);
+// append an element 'newval' into the list.
+
+void list_append_int(list * self, int64_t newval);
+// append an integer 'newval' into the list.
+
+void list_pop_by_node(list * self, node * pop_node);
+// remove the node 'pop_node' from list.
+
+void list_pop_by_index(list * self, size_t index);
+// remove the node at index 'index' from list.
+
+void list_free(list * self);
+// free the list. only frees every node and struct list;
+
+char * list_to_str(list * self);
+// return a str, pretending every element inside the list is a char, by linking each element.
+
 
 
 list * new_list(void) {
@@ -218,6 +284,14 @@ list * new_list(void) {
 	return self;
 }
 
+list * new_list_init(uint32_t param_len, char * params[]) {
+	list * self = new_list();
+	for (uint32_t i = 0; i < param_len; i++) {
+		list_append(self, params[i]);
+	}
+	return self;
+}
+
 node * list_get_node(list * self, size_t index) {
 	if (index == 0) {
 		return self->first_node;
@@ -229,7 +303,8 @@ node * list_get_node(list * self, size_t index) {
 	size_t start_index;
 	if (index >= self->last_visit_node_index) {
 		start_index = self->last_visit_node_index;
-	} else {
+	}
+	else {
 		start_index = 0;
 		self->last_visit_node = self->first_node;
 	}
@@ -263,7 +338,7 @@ void list_append_int(list * self, int64_t newval) {
 	list_append(self, tmp.void_s);
 }
 
-void list_pop_node(list * self, node * pop_node) {
+void list_pop_by_node(list * self, node * pop_node) {
 	node * next_node = pop_node->next_node;
 	void * tmp_val = pop_node->value;
 
@@ -281,8 +356,8 @@ void list_pop_node(list * self, node * pop_node) {
 	free(next_node);
 }
 
-void list_pop(list * self, size_t index) {
-	list_pop_node(self, list_get_node(self, index));
+void list_pop_by_index(list * self, size_t index) {
+	list_pop_by_node(self, list_get_node(self, index));
 }
 
 void list_free(list * self) {
@@ -330,17 +405,6 @@ __attribute__((unused)) void list_print_str(list * self) {
 	printf("\n");
 }
 
-char * get_input(void) {
-	list * input_list = new_list();
-	char * return_val;
-	int c;
-	while ((c = getchar()) != '\n') {
-		list_append_int(input_list, c);
-	}
-	return_val = list_to_str(input_list);
-	list_free(input_list);
-	return return_val;
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // CSV reading:
@@ -387,9 +451,11 @@ typedef struct {
 void csv_print_element(csv_element * element_) {
 	if (element_->var_type == CSV_FLOAT_) {
 		printf("%lf", element_->value.float_);
-	} else if (element_->var_type == CSV_INT_) {
+	}
+	else if (element_->var_type == CSV_INT_) {
 		printf("%li", element_->value.int_);
-	} else {
+	}
+	else {
 		printf("%s", element_->value.string_);
 	}
 }
@@ -411,10 +477,16 @@ void csv_free_element(csv_element * self) {
 	free(self);
 }
 
+void csv_free_elements(list * column) {
+	for (uint32_t i = 0; i < column->len; i++) {
+		csv_free_element($(column, i));
+	}
+}
+
 csv_sheet * csv_sheet_create(char * sheet_name, list * sheet_titles, uint32_t sheet_index_row) {
 	csv_sheet * self = malloc(sizeof(csv_sheet));
 	self->element_count = 0;
-	self->sheet_name = malloc(strlen(sheet_name));
+	self->sheet_name = malloc(sizeof(char) * (strlen(sheet_name) + 1));
 	strcpy(self->sheet_name, sheet_name);
 	self->sheet_titles = sheet_titles;
 	self->sheet_index_row = sheet_index_row;
@@ -445,6 +517,16 @@ void csv_sheet_append_by_name(csv_table * self, char * csv_sheet_name, list * co
 	csv_sheet_append(dict_find(self->table_dict, csv_sheet_name), column);
 }
 
+void csv_sheet_pop(csv_sheet * self, uint32_t index) {
+	list * my_column = $(self->sheet_element, index);
+	char * hash_element = $(my_column, self->sheet_index_row);
+	dict_del(self->sheet_index_dict, hash_element);
+	csv_free_elements(my_column);
+	list_pop_by_index(self->sheet_element, index);
+	self->element_count -= 1;
+
+}
+
 list * csv_sheet_get_column_by_name(csv_sheet * self, char * index_name) {
 	return dict_find(self->sheet_index_dict, index_name);
 }
@@ -464,7 +546,6 @@ csv_table * new_csv_table(void) {
 	return self;
 }
 
-
 void csv_table_append_sheet(csv_table * self, csv_sheet * _element) {
 	dict_store(self->table_dict, _element->sheet_name, _element);
 	list_append(self->table_list, _element);
@@ -474,7 +555,8 @@ csv_sheet * csv_table_get_sheet_by_name(csv_table * self, char * sheet_name) {
 	return dict_find(self->table_dict, sheet_name);
 }
 
-list * p__csv_split_line(char * line, char ** buf_con) {
+
+list * p__csv_parse_split_line(char * line, char ** buf_con) {
 	// input: "hi, 33.3"
 	// return: [['h', 'i'], ['3', '3', '.', '3']]
 
@@ -508,14 +590,16 @@ list * p__csv_split_line(char * line, char ** buf_con) {
 			list_append(result_list, buf);
 			buf = new_list();
 			is_start_element = false;
-		} else {
+		}
+		else {
 			is_start_element = true;
 			list_append_int(buf, line[i]);
 		}
 	}
 	if (buf->len > 0) {
 		list_append(result_list, buf);
-	} else {
+	}
+	else {
 		free(buf);
 	}
 	* buf_con = line + i;
@@ -534,19 +618,21 @@ csv_element * p__csv_parse_element(list * element) {
 	current_element->value.int_ = strtoll(element_str, & endptr, 10);
 	if (endptr[0] == '\0') {
 		current_element->var_type = CSV_INT_;
-	} else {
+	}
+	else {
 		current_element->value.float_ = strtod(element_str, & endptr);
 		if (endptr[0] == '\0') {
 			current_element->var_type = CSV_FLOAT_;
-		} else {
+		}
+		else {
 			// it is a str
 			node * last_node;
 			// removing str at the first and the end
 
 			if (element_str[0] == '"' &&
 			    element_str[element->len - 1] == '"') {
-				list_pop(element, 0);
-				list_pop(element, element->len - 1);
+				list_pop_by_index(element, 0);
+				list_pop_by_index(element, element->len - 1);
 			}
 			// parse "" as "
 			node * current_node;
@@ -557,14 +643,16 @@ csv_element * p__csv_parse_element(list * element) {
 				element_char = (int) (uint64_t) current_node->value;
 				if (element_char == '"') {
 					if (is_quote) {
-						list_pop_node(element, current_node);
+						list_pop_by_node(element, current_node);
 						element->last_visit_node = last_node;
 						element->last_visit_node_index = i - 1;
-					} else {
+					}
+					else {
 						last_node = current_node;
 					}
 					is_quote = !is_quote;
-				} else {
+				}
+				else {
 					is_quote = false;
 				}
 			}
@@ -577,7 +665,7 @@ csv_element * p__csv_parse_element(list * element) {
 }
 
 list * p__csv_parse_line(char * line, char ** buf_con) {
-	list * list_line = p__csv_split_line(line, buf_con);
+	list * list_line = p__csv_parse_split_line(line, buf_con);
 	list * list_result = new_list();
 	list * list_element;
 
@@ -617,7 +705,6 @@ list * p__csv_read_raw_table(char * file_name) {
 	return csv_list;
 }
 
-
 sheet_properties * p__csv_try_read_properties(list * column) {
 	sheet_properties * tmp = NULL;
 	csv_element * first_item = list_get_node(column, 0)->value;
@@ -632,8 +719,8 @@ sheet_properties * p__csv_try_read_properties(list * column) {
 			csv_free_element(first_item);
 			csv_free_element(list_get_node(column, column->len - 1)->value);
 
-			list_pop(column, 0);
-			list_pop(column, column->len - 1);
+			list_pop_by_index(column, 0);
+			list_pop_by_index(column, column->len - 1);
 
 			tmp->sheet_titles = column;
 		}
@@ -664,7 +751,7 @@ void csv_open(char * file_name, csv_table * table) {
 		}
 		target_sheet_name = __get_node_item(csv_raw_column, 0).string_;
 		tmp = list_get_node(csv_raw_column, 0)->value;
-		list_pop(csv_raw_column, 0);
+		list_pop_by_index(csv_raw_column, 0);
 		csv_sheet_append_by_name(table, target_sheet_name, csv_raw_column);
 		csv_free_element(tmp);
 	}
@@ -679,16 +766,33 @@ void csv_print_column(list * column) {
 	printf("\n");
 }
 
-void csv_print_sheet(csv_table * self, char * sheet_name) {
+void csv_print_sheet(csv_table * self, char * sheet_name, char * index_prefix, char * index_name) {
 	csv_sheet * self_sheet = csv_table_get_sheet_by_name(self, sheet_name);
+	printf("%s\t", index_name);
 	csv_print_column(self_sheet->sheet_titles);
 	for (uint32_t column_number = 0; column_number < self_sheet->element_count; column_number++) {
+		printf(index_prefix, column_number + 1);
+		printf("\t");
 		csv_print_column(csv_sheet_get_column_by_index(self_sheet, column_number));
 	}
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Utility functions:
 
+
+char * get_input(void) {
+	list * input_list = new_list();
+	char * return_val;
+	int c;
+	while ((c = getchar()) != '\n') {
+		list_append_int(input_list, c);
+	}
+	return_val = list_to_str(input_list);
+	list_free(input_list);
+	return return_val;
+}
 
 bool input_yn_question(char * message) {
 	char * input;
@@ -731,9 +835,10 @@ char * input_option_question(
 int main(void) {
 	csv_table * my_table = new_csv_table();
 	csv_open("test.csv", my_table);
-	csv_print_sheet(my_table, "MY_TABLE");
+	csv_print_sheet(my_table, "MY_TABLE", "%u", "INDEX");
 
 	list * my_column = csv_sheet_get_column_by_name(csv_table_get_sheet_by_name(my_table, "MY_TABLE"), "2LOf2");
 	csv_print_element($(my_column, 1));
-
 }
+
+
